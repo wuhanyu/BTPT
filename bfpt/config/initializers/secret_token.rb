cb1292329b71fb0800fcabacca3f31830852055c
SimonWu <wuhanyu@gmail.com>
2014-06-21 21:40:44 +0800

    first blood

---
+++ bfpt/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bfpt::Application.config.secret_key_base = 'f8b7891140deb49cac2efb55e73b2d3fc2989940845d73877e3ce18d88a0d37108f77b32f18c187b069a132d10949f6f58d9295163cc460a5ac17eefd00f6b3e'
